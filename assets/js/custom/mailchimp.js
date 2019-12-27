bf9a3f47155d6a5a8cc812cfc80c86d6b2db864f
Gonçalo Henriques <gonelf@gmail.com>
2019-12-27 18:05:11 +0000

    add mailchimp back

--- assets/js/custom/mailchimp.js
+++ assets/js/custom/mailchimp.js
@@ -1,1 +1,4 @@
 window.dojoRequire(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us4.list-manage.com","uuid":"b9b0fb71d1f83b3d91bf76fb3","lid":"8a7c953072","uniqueMethods":true}) })
+$('#mc-form').ajaxChimp({
+    url: 'http://mc.us4.list-manage.com/subscribe/post?u=b9b0fb71d1f83b3d91bf76fb3&id=8a7c953072'
+});
