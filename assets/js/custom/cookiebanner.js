abc8968f285f6138d2b6b8bebadf1868652a60c6
Gonçalo Henriques <gonelf@gmail.com>
2019-12-27 17:19:54 +0000

    cookie consent

---
+++ assets/js/custom/cookiebanner.js
@@ -0,0 +1,10 @@
+(function () {
+  var s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.async = true;
+  s.src = 'https://app.termly.io/embed.min.js';
+  s.id = '2773aed6-1cc4-4599-80e9-97a20e0391b2';
+  s.setAttribute("data-name", "termly-embed-banner");
+  var x = document.getElementsByTagName('script')[0];
+  x.parentNode.insertBefore(s, x);
+})();
