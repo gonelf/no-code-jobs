8f961469b2b61226a51b1a256dd96e1325cd9617
Gonçalo Henriques <gonelf@gmail.com>
2019-12-27 16:33:53 +0000

    add drift
    clean up js scripts to single files

---
+++ assets/js/custom/jotform.js
@@ -0,0 +1,11 @@
+var JFL_193544731007051 = new JotformFeedback({
+  formId: '193544731007051',
+  base: 'https://form.jotform.com/',
+  windowTitle: 'No-Code Job Post',
+  background: '#FFA500',
+  fontColor: '#FFFFFF',
+  type: 'false',
+  height: 500,
+  width: 700,
+  openOnLoad: false
+});
---
+++ assets/js/custom/mailchimp.js
@@ -0,0 +1,1 @@
+window.dojoRequire(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us4.list-manage.com","uuid":"b9b0fb71d1f83b3d91bf76fb3","lid":"8a7c953072","uniqueMethods":true}) })
