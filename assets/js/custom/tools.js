8a65124cfadd64eef53da23be86aa34d1039b2c1
Gonçalo Henriques <gonelf@gmail.com>
2020-01-06 00:21:58 +0000

    add tool page

--- assets/js/custom/tools.js
+++ assets/js/custom/tools.js
@@ -1,3 +1,16 @@
+function populateTool(tool) {
+  console.log(tool);
+  $("#title").html("About "+tool['name']);
+  $("#overview").html(tool['overview']);
+  $("#box_title").html(tool['title']);
+  $("#tool_logo").attr('src', tool['logo']);
+  $("#tool_logo").attr('alt', tool['name']);
+  $("#tags").html(tool['tags'].split(",").join(", "));
+  $("#link").attr('href', tool['url']);
+  $("#link").html(tool['url']);
+  $("#tool").show();
+}
+
 var url = "https://script.google.com/macros/s/AKfycbxmHiRBIhd7ErXuJlm8QiweTth46ZxHKJuNRjMp7EylT9faGw/exec?sheet=tools";
 
 $.getJSON( url, function( data ) {
@@ -6,10 +19,15 @@ $.getJSON( url, function( data ) {
   if (count > 0) {
     $.each( data, function( key, val ) {
       var items = [];
+      var tool_param = getUrlParameter("tool");
+      var tool_info = {}
+
       $.each( val, function( i_key, tool ) {
 
+        if (tool_param && tool_param.toLowerCase() == tool['name'].toLowerCase()) tool_info = tool;
+
         items.push('<div class="col-xl-4 col-lg-6 col-md-4 col-sm-6 col-12">'+
-                        '<a href="company-single.html" class="company-list">'+
+                        '<a href="tool.html?tool='+tool['name']+'" class="company-list">'+
                             '<span class="company-logo"><img src="'+tool['logo']+'" style="width: 70px;" alt="company-1"></span>'+
                             '<h6 class="title">'+tool['name']+'</h6>'+
                             //'<span class="open-job">2 open positions</span>'+
@@ -18,7 +36,12 @@ $.getJSON( url, function( data ) {
                     '</div>');
       });
 
-      $( ".company-list-wrap" ).append(items.join(""));
+      if (Object.keys(tool_info).length > 0) {
+        populateTool(tool_info);
+      }
+      else {
+        $( ".company-list-wrap" ).append(items.join(""));
+      }
     });
   }
   else {
