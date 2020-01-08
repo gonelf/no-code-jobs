6884de7c62920c297cf87c5064c20b05a59bc032
Gonçalo Henriques <gonelf@gmail.com>
2020-01-08 13:38:09 +0000

    Update jobpostform.js

--- assets/js/custom/jobpostform.js
+++ assets/js/custom/jobpostform.js
@@ -1,5 +1,14 @@
+jQuery.loadScript = function (url, callback) {
+    jQuery.ajax({
+        url: url,
+        dataType: 'script',
+        success: callback,
+        async: true
+    });
+}
+
 $(".cognito").click(function(){
-  $.getScript("https://services.cognitoforms.com/s/nPwE6ocbek6wVFrZxleMwg", function(){
+  $.loadScript("https://services.cognitoforms.com/s/nPwE6ocbek6wVFrZxleMwg", function(){
     console.log("loaded");
     Cognito.load("forms", { id: "1" });
   });
