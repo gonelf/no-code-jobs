45aee64bc1eb4d6b8c20efd8a013144c892032ca
Gonçalo Henriques <gonelf@gmail.com>
2020-01-09 20:30:45 +0000

    test stripe new form

---
+++ assets/js/custom/stripe.js
@@ -0,0 +1,47 @@
+var stripe = (getUrlParameter("private") == true) ? Stripe('pk_test_zEz6HMIIxdVpTqd8e09nfc1600hNjtEOqv') : Stripe('pk_live_oDYKR8kGsKaX40QaPUaVDEMl00Csg3Z8mP');
+var elements = stripe.elements();
+
+// Create an instance of the card Element.
+var card = elements.create('card');
+
+// Add an instance of the card Element into the `card-element` <div>.
+card.mount('#card-element');
+
+// Handle real-time validation errors from the card Element.
+card.addEventListener('change', function(event) {
+  var displayError = document.getElementById('card-errors');
+  if (event.error) {
+    displayError.textContent = event.error.message;
+  } else {
+    displayError.textContent = '';
+  }
+});
+
+// Handle form submission.
+var form = document.getElementById('payment-form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  stripe.createToken(card).then(function(result) {
+    if (result.error) {
+      // Inform the user if there was an error.
+      var errorElement = document.getElementById('card-errors');
+      errorElement.textContent = result.error.message;
+    } else {
+      // Send the token to your server.
+      stripeTokenHandler(result.token);
+    }
+  });
+});
+
+// Submit the form with the token ID.
+function stripeTokenHandler(token) {
+  // Insert the token ID into the form so it gets submitted to the server
+  var form = document.getElementById('payment-form');
+  var hiddenInput = document.createElement('input');
+  hiddenInput.setAttribute('type', 'hidden');
+  hiddenInput.setAttribute('name', '_stripe_token');  // <-- important
+  hiddenInput.setAttribute('value', token.id);
+  form.appendChild(hiddenInput);
+  // Submit the form
+  form.submit();
+}
