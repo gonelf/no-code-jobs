ff7f95588328840986bf7b8d62f3716665644951
Gonçalo Henriques <gonelf@gmail.com>
2020-01-14 00:12:02 +0000

    update firebase init location

---
+++ assets/js/custom/firebase_init.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAixUDo3YBhwKtfh-_QB1PYVGpHqx7r200",
+  authDomain: "nocodery.firebaseapp.com",
+  databaseURL: "https://nocodery.firebaseio.com",
+  projectId: "nocodery",
+  storageBucket: "nocodery.appspot.com",
+  messagingSenderId: "463330803215",
+  appId: "1:463330803215:web:3d19f661465796844bed7a",
+  measurementId: "G-KRLQ5YM9ZK"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
