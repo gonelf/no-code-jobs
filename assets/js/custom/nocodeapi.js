1fd7ef8474cff3b4f7205ba8ea11dd0644e0d8b4
Gonçalo Henriques <gonelf@gmail.com>
2020-01-18 01:49:13 +0000

    register member

--- assets/js/custom/nocodeapi.js
+++ assets/js/custom/nocodeapi.js
@@ -1,4 +1,6 @@
-async function registerUser(username, email) {
+async function registerUser(member username, email) {
+  var username = member.username;
+  var email = member.email;
     try {
         const response = await fetch("https://v1.nocodeapi.com/nocodery/airtable/JbqRxeaBwbmMXsBU", {
             method: "post",
@@ -9,7 +11,7 @@ async function registerUser(username, email) {
         });
         const json = await response.json();
         console.log("Success:", JSON.stringify(json[0]['fields']['record_id']));
-        return json[0]['fields']['record_id'];
+        updateProfile(member, {"register_id": json[0]['fields']['record_id']})
     } catch (error) {
         console.error("Error:", error);
         return false;
