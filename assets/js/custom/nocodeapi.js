0796bde016eaf34faf65103632c84f627f7e6e24
Gonçalo Henriques <gonelf@gmail.com>
2020-01-18 02:14:07 +0000

    update

--- assets/js/custom/nocodeapi.js
+++ assets/js/custom/nocodeapi.js
@@ -11,8 +11,7 @@ async function registerUser(member, callback) {
         });
         const json = await response.json();
         console.log("Success:", JSON.stringify(json[0]['fields']['record_id']));
-        console.log(member);
-        return callback(member, {"register_id": json[0]['fields']['record_id']})
+        return callback(json[0]['fields']['record_id'])
     } catch (error) {
         console.error("Error:", error);
         return false;
