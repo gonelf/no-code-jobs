6dfa20028e0ca4dc64e13b99764d790ef3e147d4
Gonçalo Henriques <gonelf@gmail.com>
2020-01-23 15:29:55 +0000

    sheety api test

---
+++ assets/js/custom/sheetyAPI.js
@@ -0,0 +1,64 @@
+async function registerUser(member, callback) {
+  var username = member.username;
+  var email = member.email;
+    try {
+        const response = await fetch("https://v2-api.sheety.co/a0ec0d951abaa3c46c358969a6b2f696/nocodeDb/users/", {
+            method: "post",
+			      body: JSON.stringify([{"username":username,"email":email}]),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        const json = await response.json();
+        return callback(json[0]['fields']['record_id'])
+    } catch (error) {
+        console.error("Error:", error);
+        return false;
+    }
+}
+
+async function updateRecord(record, callback) {
+    try {
+        const response = await fetch("https://v1.nocodeapi.com/nocodery/airtable/JbqRxeaBwbmMXsBU", {
+            method: "patch",
+			body: JSON.stringify([record]),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        const json = await response.json();
+        return callback(json);
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+async function getUser(record_id, callback) {
+    try {
+        const response = await fetch("https://v2-api.sheety.co/a0ec0d951abaa3c46c358969a6b2f696/nocodeDb/users/"+record_id, {
+            method: "get",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        const json = await response.json();
+        return callback(json)
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+async function getUsers(callback) {
+    try {
+        const response = await fetch("https://v1.nocodeapi.com/nocodery/airtable/JbqRxeaBwbmMXsBU", {
+            method: "get",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        const json = await response.json();
+        return callback(json)
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
