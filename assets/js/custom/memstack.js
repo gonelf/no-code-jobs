f004f66b6b14c74bb558f13065e4d14fe22a73df
Gonçalo Henriques <gonelf@gmail.com>
2020-01-23 16:04:45 +0000

    hack memberstack

--- assets/js/custom/memstack.js
+++ assets/js/custom/memstack.js
@@ -1,3 +1,10 @@
 function updateRegisterId (member, obj) {
    member.updateProfile(obj, false)
 }
+MemberStack.onReady.then(function(member) {
+   // check if member is logged in
+   console.log(member.loggedIn);
+   if (member.loggedIn){
+     $("#ms-warning-box").hide();
+   }
+})
