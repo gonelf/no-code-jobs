f196a0987823aae294b6032477c0b8e0ddbad9ee
Gonçalo Henriques <gonelf@gmail.com>
2020-01-24 00:27:50 +0000

    Update sheetyAPI.js

--- assets/js/custom/sheetyAPI.js
+++ assets/js/custom/sheetyAPI.js
@@ -2,14 +2,14 @@ async function registerUser(member, callback) {
   var username = member.username;
   var email = member.email;
   var id = member.id
-  var body = {
-          "id": id,
-          "username":username,
-          "email":email,
-          "created_date": new Date().toISOString(),
-          "updated_date": new Date().toISOString()
-        
-    }
+  var body = {}
+  body['user'] = {}
+  body['user']['id'] = idea
+  body['user']['email'] = email
+  body['user']['username'] = username
+  body['user']['created_date'] = new Date().toISOString()
+  body['user']['updated_date'] = new Date().toISOString()
+  
     console.log(body);
     try {
         const response = await fetch("https://v2-api.sheety.co/a0ec0d951abaa3c46c358969a6b2f696/nocodeDb/users", {
